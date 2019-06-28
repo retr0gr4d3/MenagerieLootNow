fc44bb6090f87b6b3653d9f98545f1406165fca7
retr0gr4d3 <legendbreakerjake@gmail.com>
2019-06-28 14:55:29 +0100

    Add files via upload

---
+++ MenagerieLootNow.py
@@ -0,0 +1,79 @@
+1. --Sub MGs--
+CalusMiniTool
+TracklessWaste
+HardTruths
+BadReputation
+
+2. --Snipers--
+TwilightOath
+Beloved
+FateCriesFoul
+DreadedVenture
+
+3. --Rocket Launchers--
+BadOmens
+ZenobiaD
+Sleepless
+
+3.1 --Heavy MGs--
+FixedOdds
+
+4. --Hand Cannons--
+Trust
+Austringer
+WakingVigil
+PribinaD
+
+5. --Sidearms--
+DrangBaroque
+TheLastDance
+AnonymousAutumn
+SmugglersWord
+
+6. --Fusion Rifles--
+ProeliumFR3
+MainIngredient
+TheEpicurean
+ErentilFR4
+
+7. --Shotguns--
+Badlander
+ParcelOfStardust
+ImperialDecree
+DustRockBlues
+
+
+
+def pickLoot():
+    cat01 = input('''Chalice of Opulence loot pool:
+
+1 - Sub MGs
+2 - Snipers
+3 - Rocket Launchers
+3.1 - Heavy MGs
+4 - Hand Cannons
+5 - Sidearms
+6 - Fusion Rifles
+7 - Shotguns
+
+Please pick a number from 1-7: ''')
+    if cat01 == '1':
+        subMGs()
+    elif cat01 == '2':
+        snipers()
+    elif cat01 == '3':
+        rocketLaunchers()
+    elif cat01 == '3.1':
+        heavyMGs()
+    elif cat01 == '4':
+        handCannons()
+    elif cat01 == '5':
+        sidearms()
+    elif cat01 == '6':
+        fusionRifles()
+    elif cat01 == '7':
+        shotguns()
+
+    else:
+        print('Invalid answer. Please try again.')
+        pickLoot()
